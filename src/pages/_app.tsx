216176bcbb36718b0cf0d3363ff7dfb44bf12cae
Kevin Simorangkir <104813623+kevinsimorangkir21@users.noreply.github.com>
2025-02-15 23:47:20 +0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,14 @@ import NProgress from 'nprogress'
 import Router from "next/router";
 import type { AppProps } from 'next/app'
 import React from 'react';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   React.useEffect(() => {
     NProgress.configure({ showSpinner: false });
-    
+
     const handleRouteStart = () => NProgress.start();
     const handleRouteDone = () => NProgress.done();
 
@@ -17,14 +20,20 @@ function MyApp({ Component, pageProps }: AppProps) {
     Router.events.on("routeChangeError", handleRouteDone);
 
     return () => {
-      // Make sure to remove the event handler on unmount!
       Router.events.off("routeChangeStart", handleRouteStart);
       Router.events.off("routeChangeComplete", handleRouteDone);
       Router.events.off("routeChangeError", handleRouteDone);
     };
   }, []);
 
-  return <Component {...pageProps} />
+  // Redirect ke halaman "Coming Soon"
+  React.useEffect(() => {
+    if (router.pathname !== "/coming-soon") {
+      router.replace("/coming-soon");
+    }
+  }, [router]);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
