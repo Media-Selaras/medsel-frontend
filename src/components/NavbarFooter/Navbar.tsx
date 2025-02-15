7a65a115d175b2d5bff6ead5c6a26aa3f50bfb3f
Kevin Simorangkir <104813623+kevinsimorangkir21@users.noreply.github.com>
2025-02-15 15:41:34 +0700

    Update Navbar.tsx

--- src/components/NavbarFooter/Navbar.tsx
+++ src/components/NavbarFooter/Navbar.tsx
@@ -86,7 +86,7 @@ export default function Navbar() {
             <Disclosure.Panel className="lg:hidden absolute top-16 left-0 w-full bg-white shadow-md">
               <div className="px-2 pt-2 pb-3 space-y-1">
                 <Link href="/" className="block px-3 py-2 text-gray-900 hover:bg-gray-200 rounded-md">Beranda</Link>
-                <Link href="/artikel" className="block px-3 py-2 text-gray-900 hover:bg-gray-200 rounded-md">Artikel</Link>
+                {/* <Link href="/artikel" className="block px-3 py-2 text-gray-900 hover:bg-gray-200 rounded-md">Artikel</Link> */}
                 <Informasi />
                 <TentangKami />
               </div>
