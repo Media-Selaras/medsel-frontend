4a2eb4dd1ac85acacc8e849b20da8ef24289414a
Kevin Simorangkir <104813623+kevinsimorangkir21@users.noreply.github.com>
2025-03-08 22:50:44 +0700

    Update date.ts

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -14,12 +14,11 @@ const months = [
   "Desember",
 ];
 
-export const Date = (date: any) => {
-  const blogDate = date.split("-");
-  const tanggal = parseInt(blogDate[2]);
-  let bulan = blogDate[1];
-  const tahun = parseInt(blogDate[0]);
-  bulan = months[parseInt(bulan)];
-  date = `${tanggal} ${bulan} ${tahun}`;
-  return date;
+export const formatDate = (dateString: string): string => {
+  const blogDate = dateString.split("-");
+  const tanggal = parseInt(blogDate[2], 10);
+  const bulan = months[parseInt(blogDate[1], 10)];
+  const tahun = parseInt(blogDate[0], 10);
+
+  return `${tanggal} ${bulan} ${tahun}`;
 };
